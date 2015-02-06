4b977b42e77ee250516429ac5a8101bfe7093dee
Zander Allen <ztrain01@gmail.com>
2015-02-05 19:36:12 -0700

    Successfully got the DOM to work

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ $(document).ready(function() {
         return $.ajax({
             url: 'http://reqr.es/api/users?page=1',
             method: 'GET',
-            success: function(data) {
-                console.log(data)
+            success: function(res) {
+                insertData(res.data)
         }
         });
     });
-
+var insertData = function(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      $('#userInfo' + (i + 1)).html('<div>' +
+        'User Info:' +
+        '<li>' +
+        'First name: ' + arr[i].first_name +
+        '</li>' +
+        '<li>' +
+        'Last name: ' + arr[i].last_name +
+        '</li>' +
+        '<hr>' +
+        '</div>'
+      )
+    };
+  }
 });
